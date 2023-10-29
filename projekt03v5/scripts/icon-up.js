567e46949aa84e27240be461c754dfc341e09df4
Adéla Kohoutková <130575660+Craterisia@users.noreply.github.com>
2023-10-29 13:03:16 +0100

    Delete projekt03v5 directory
    New version ahead

--- projekt03v5/scripts/icon-up.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", () => {
-  if (window.scrollY >= 400) {
-    iconUp.classList.add("icon-up-light");
-    iconUp.classList.remove("invisible");
-  } else {
-    iconUp.classList.add("invisible");
-    iconUp.classList.remove("icon-up-light");
-  }
-});
